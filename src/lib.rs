3f718c9ab5fd9084112a62a08a84a87e65d7bf53
Pontus Laestadius <pontus.laestadius@gmail.com>
2017-12-24 13:14:34 +0100

    the worldst worst opcodes matcher. Merry christmas. God jul

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(slice_patterns)]
+#![feature(inclusive_range_syntax)]
+
 
 pub mod register;
 pub mod instructions;
@@ -55,23 +58,36 @@ pub struct SmartBinary {
 impl SmartBinary {
     pub fn new(byte: u8) -> SmartBinary {
         // TODO
-        let formatted = format!("{:b}", byte);
+
+        let bytes = format!("{:b}", byte);
+
+        let formatted = if bytes.len() != 8 {
+            let mut extra = String::new();
+            for _ in bytes.len()...8  {
+                extra.push('0');
+            }
+            extra.push_str(bytes.as_str());
+            extra
+        } else {
+            bytes
+        };
 
         let mut formatted_chars = formatted.chars();
 
         let o = |x| x == '1';
 
+        // nth consumes the elements, so calling 0 on each one returns different elements:
+        // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.nth
         SmartBinary {
             zer: o(formatted_chars.nth(0).unwrap()),
-            one: o(formatted_chars.nth(1).unwrap()),
-            two: o(formatted_chars.nth(2).unwrap()),
-            thr: o(formatted_chars.nth(3).unwrap()),
-            fou: o(formatted_chars.nth(4).unwrap()),
-            fiv: o(formatted_chars.nth(5).unwrap()),
-            six: o(formatted_chars.nth(6).unwrap()),
-            sev: o(formatted_chars.nth(7).unwrap()),
+            one: o(formatted_chars.nth(0).unwrap()),
+            two: o(formatted_chars.nth(0).unwrap()),
+            thr: o(formatted_chars.nth(0).unwrap()),
+            fou: o(formatted_chars.nth(0).unwrap()),
+            fiv: o(formatted_chars.nth(0).unwrap()),
+            six: o(formatted_chars.nth(0).unwrap()),
+            sev: o(formatted_chars.nth(0).unwrap()),
         }
-
     }
 
     pub fn get(&self, bit: u8) -> bool {
@@ -88,9 +104,53 @@ impl SmartBinary {
         }
     }
 
+    pub fn x_y_z_p_q(&self) -> [u8; 5] {
+
+        let orev = |x: bool| {
+            if x {
+                1
+            } else {
+                0
+            }
+        };
+
+        // x = the opcode's 1st octal digit (i.e. bits 7-6)
+        let x = octal_digit_from_binary_list(&[
+            orev(self.sev),
+            orev(self.six)
+        ]);
+
+        // y = the opcode's 2nd octal digit (i.e. bits 5-3)
+        let y = octal_digit_from_binary_list(&[
+            orev(self.fiv),
+            orev(self.fou),
+            orev(self.thr)
+        ]);
+
+        // z = the opcode's 3rd octal digit (i.e. bits 2-0)
+        let z = octal_digit_from_binary_list(&[
+            orev(self.two),
+            orev(self.one),
+            orev(self.zer)
+        ]);
+
+        // p = y rightshifted one position (i.e. bits 5-4)
+        let p = octal_digit_from_binary_list(&[
+            orev(self.fiv),
+            orev(self.fou),
+        ]);
+
+        // q = y modulo 2 (i.e. bit 3)
+        let q = octal_digit_from_binary_list(&[
+            orev(self.thr),
+        ]);
+
+        [x,y,z,p,q]
+    }
+
 }
 
-pub fn rom_exec(mut file: &mut File) {
+pub fn rom_exec(file: &mut File) {
     let mut buffer: Vec<u8> = Vec::new();
     let rom_size = file.read_to_end(&mut buffer).unwrap();
 
@@ -114,7 +174,12 @@ pub fn rom_exec(mut file: &mut File) {
         bytes  += 1;
 
         let step = session.step();
-        println!("HEX: {:#X} U8: {} BINARY: {:b}", step, step, step);
+
+        let sb = SmartBinary::new(step.clone());
+
+        println!("unprefixed_opcodes: {:?}", unprefixed_opcodes(sb));
+
+        //println!("HEX: {:#X} U8: {} BINARY: {:b}", step, step, step);
 
     }
 }
@@ -127,25 +192,47 @@ pub fn opcode_table(byte: u8) {
 
 }
 
-pub fn unprefixed_opcodes(binary: SmartBinary) {
-    let orev = |x: bool| {
-        if x {
-            1
-        } else {
-            0
-        }
-    };
 
-    // Gets the octal digit x.
-    let x = orev(binary.sev)*2 + orev(binary.six);
+pub fn unprefixed_opcodes<'a>(binary: SmartBinary) -> &'a str {
+
+    // Uses experimental splice patterning.
+    let [x,y,z,p,q] = binary.x_y_z_p_q();
+
+    println!("{},{},{},{},{}", x,y,z,p,q);
 
     match x {
-        1 => 1,
-        2 => 2,
-        3 => 3,
-        4 => 4,
-        _ => panic!("Invalid X value: {}", x),
-    };
+        0 => {
+            match z {
+                0 => {
+                    match y {
+                        0 => "NOP",
+                        1 => "EX AF, AF'",
+                        2 => "undefined",
+                        3 => "undefined",
+                        4 => "undefined",
+                        5 => "undefined",
+                        6 => "undefined",
+                        7 => "undefined",
+                        _ => "Invalid Y value",
+                    }
+                }
+                1 => "undefined",
+                2 => "undefined",
+                3 => "undefined",
+                4 => "undefined",
+                5 => "undefined",
+                6 => "undefined",
+                7 => "undefined",
+                _ => "Invalid Z value",
+            }
+
+        }
+        1 => "undefined",
+        2 => "undefined",
+        3 => "undefined",
+        4 => "undefined",
+        _ => "Invalid X value",
+    }
 }
 
 pub fn octal_digit_from_binary_list(list: &[u8]) -> u8 {
@@ -166,5 +253,5 @@ fn test_octal_digit() {
     assert_eq!(octal_digit_from_binary_list(&[1,1,1,1,1,1,1]), 127);
     assert_eq!(octal_digit_from_binary_list(&[1,1,1,1,1,1,0]), 126);
     assert_eq!(octal_digit_from_binary_list(&[0,1,1,1,1,1,0]), 126-64);
-    
+
 }
